1a49c0be54f5bb6c094714e41ca17a7b3a6a6a51
zhouJie <1012172055@qq.com>
2020-08-06 13:39:02 +0800

    Update ReconnetWebsocket.js

--- ReconnetWebsocket.js
+++ ReconnetWebsocket.js
@@ -2,18 +2,27 @@
  * @description 基于sockjs+stompjs的websocket自动重连工具类
  * 封装内容：
  * 1. 一个类对象只使用一个sessionId，断开重连后也是使用这一个sessionId进行重连
- * 2. 对连接状态做封装，connected：true,connecting,false，防止重复调用connect导致重复连接
+ * 2. 对连接状态做封装，防止重复调用connect导致重复连接
  * 3. 断线重连机制
  * 4. stomp内执行websocket的onclose回调时不会把错误信息带给回调，这里做了封装，会把原生websocket错误对象以参数形式传给回调
+ * 5. 当sockjs处于连接中状态时，disconnect处理
  * @author zhoujie
  */
 import SockJs from '@aicc/assets/sockjs'
 import { generateRandom } from '@aicc/utils'
 import Stomp from 'stompjs'
+const logger = require('@aicc/utils/logger').logger('ReconnetWebsocket')
+
+// 连接状态
+const CONNECTING = 'connecting'
+const CONNECTED = 'connected'
+const FAILD = 'failed'
+const DISCONNECTING = 'disconnecting'
+const DISCONNECTED = 'disconnected'
 
 export default class ReconnetWebsocket {
   constructor(url) {
-    this.connected = false
+    this.status = DISCONNECTED
     this.url = url
     this.stompClient = undefined
     this.timeout = null
@@ -21,9 +30,29 @@ export default class ReconnetWebsocket {
     this.successCb = () => {}
     this.errorCb = () => {}
     this.params = {}
+    this.shouldReconnect = true
+  }
+
+  _onConnectEnd(fn) {
+    this.connectEndFns = this.connectEndFns || []
+    this.connectEndFns.push(fn.bind(this))
+  }
+
+  _connectEnded() {
+    if (this.connectEndFns) {
+      this.connectEndFns.forEach(fn => fn())
+      this.connectEndFns = []
+    }
   }
 
   connect(params = {}, successCb = () => {}, errorCb = () => {}) {
+    /**
+     * 用户主动调用connect
+     *
+     * 这种情况需要重连以维护ws的稳定性
+     */
+    this.shouldReconnect = true
+
     return new Promise(resolve => {
       if (params instanceof Function) {
         errorCb = successCb
@@ -35,18 +64,17 @@ export default class ReconnetWebsocket {
       this.errorCb = errorCb
       this.successCb = successCb
 
-      if (this.connected === true) {
+      if (this.status === CONNECTED) {
         successCb()
         resolve()
         return
       }
 
-      if (this.connected === 'connecting') {
-        // reject('userClient正在连接中')
+      if (this.status === CONNECTING) {
         return
       }
 
-      this.connected = 'connecting'
+      this.status = CONNECTING
 
       const socket = new SockJs(this.url, [], {
         sessionId: () => {
@@ -61,12 +89,18 @@ export default class ReconnetWebsocket {
           ...(params || {}),
         },
         () => {
-          this.connected = true
+          if (this.status === DISCONNECTING) {
+            this._connectEnded()
+            return
+          }
+
+          this.status = CONNECTED
           successCb(this.stompClient)
           resolve(this.stompClient)
         },
         e => {
-          this.connected = false
+          this.status = FAILD
+          this._connectEnded()
           !errorCb.executed && errorCb(e)
           errorCb.executed = false
           this.reConnect()
@@ -84,8 +118,11 @@ export default class ReconnetWebsocket {
   }
 
   reConnect() {
+    if (!this.shouldReconnect) return
+
     this.stopTimeout()
     this.disconnect()
+    this.shouldReconnect = true
     this.timeout = setTimeout(async () => {
       await this.connect(
         this.params,
@@ -107,6 +144,16 @@ export default class ReconnetWebsocket {
     }, 1500)
   }
 
+  stopReconnect() {
+    this.shouldReconnect = false
+    this.stopTimeout()
+  }
+
+  startReconnect() {
+    this.shouldReconnect = true
+    this.reConnect()
+  }
+
   stopTimeout() {
     if (this.timeout) {
       clearTimeout(this.timeout)
@@ -115,10 +162,49 @@ export default class ReconnetWebsocket {
   }
 
   disconnect() {
-    this.connected = false
+    if (this.status === DISCONNECTING || this.status === DISCONNECTED) {
+      logger.log('ws already disconnected in util', this.status, this.url)
+      return
+    }
+
+    logger.log('disconnect ws in util', this.url)
+    this.status = DISCONNECTING
+
+    /**
+     * 主动调用disconnect
+     *
+     * 说明不需要重连
+     */
+    this.shouldReconnect = false
+
     return new Promise((resolve, reject) => {
+      const disconnected = () => {
+        this.status = DISCONNECTED
+        resolve()
+      }
+
       if (this.stompClient) {
-        this.stompClient.disconnect(resolve)
+        try {
+          this.stompClient.disconnect(disconnected)
+        } catch (e) {
+          logger.log('--------catch error disconnect------------', e)
+          /**
+           * stompClient的disconnect方法会给服务器发送一个disconnect帧，所以会调用sockjs的send方法
+           * 当ws处于连接中状态时，sockjs的send方法会抛错，导致ws关闭过程中断
+           * 而且ws此时会继续连接，最后会变成连接中状态
+           * 所以需要等待连接完毕（成功/失败）再去断开
+           */
+          if (
+            String(e).includes(
+              'InvalidStateError: The connection has not been established yet',
+            )
+          ) {
+            this._onConnectEnd(() => {
+              logger.log('------------finally disconnect----------')
+              this.stompClient.disconnect(disconnected)
+            })
+          }
+        }
       } else {
         reject('ws 对象不存在')
       }
